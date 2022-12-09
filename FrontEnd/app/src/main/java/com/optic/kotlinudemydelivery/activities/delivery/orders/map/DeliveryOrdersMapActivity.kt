16f2862b9e61c4dddc951209dd09f76ba010c85b
alex <alex.lizarazo@unmsm.edu.pe>
2022-12-09 18:18:49 -0500

    88 DONE

---
+++ FrontEnd/app/src/main/java/com/optic/kotlinudemydelivery/activities/delivery/orders/map/DeliveryOrdersMapActivity.kt
@@ -0,0 +1,11 @@
+package com.optic.kotlinudemydelivery.activities.delivery.orders.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryOrdersMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_orders_map)
+    }
+}
